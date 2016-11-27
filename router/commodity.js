944b195474166e1061ef2fc41c54ff6800fa3a94
guozhi <grozysun@gmail.com>
2016-11-27 13:48:38 +0800

    query

--- router/commodity.js
+++ router/commodity.js
@@ -3,7 +3,7 @@ var router = require("express").Router();
 const Commodity = require('../model/commodity');
 
 router.get('/', function(req, res){
-  console.log('enter commodity index...');
+  console.log('enter commodity index... query: ' + req.query.ljm);
   Commodity.queryWithLJM(req.query.ljm, function(err, commoditys) {
     console.log("err" + commoditys);
     res.send(commoditys);//登出成功后跳转到主页
