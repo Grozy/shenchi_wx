44e578ea8b85aed79e31a4561a27db9a7c4048dc
guozhi <grozysun@gmail.com>
2016-11-27 13:25:12 +0800

    db config

--- conf/db_conf.js
+++ conf/db_conf.js
@@ -2,10 +2,9 @@ module.exports = {
   mysql: {
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: 'guozhi123',
     database: 'dbms',
     port: 3306,
     queueLimit: 8
   }
 }
- 
