9162930aea5a6c1a79416c7d1e3b891fe9f4628b
guozhi <grozysun@gmail.com>
2016-11-27 13:14:41 +0800

    nothing

--- conf/app_config.js
+++ conf/app_config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'token': 'shenchi_moto',
   'appId': 'wxed60b65b7b8be3b4',
-  'appSecret': '80b3dc2e8180c33e03e52a3bcaa5edc1',
+  'appSecret': '80b3dc2e8180c33e03e52xxxxxxxx',
 };
